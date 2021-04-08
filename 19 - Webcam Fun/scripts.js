79ef0b4f7baaeed0d407fdadf5286f58696043a2
raj mangukiya <rajmangukiya00@gmail.com>
2021-04-08 12:16:22 +0530

    All files added

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,53 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(media => {
+      video.srcObject = media;
+      video.play();
+    })
+    .catch(error => {
+      console.error("it;s ERROR", error);
+  });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = rgbSplit(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+  
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=31) {
+    pixels.data[i] = pixels.data[i + 20]; // RED
+    // pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    // pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
